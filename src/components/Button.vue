16f4e9515724cec7cc5b4ad6ce066502a2ca93d2
Liudmyla Liashuk <lyudmyla.horbyk@gmail.com>
2019-11-05 00:55:42 +0200

    create button component

---
+++ src/components/Button.vue
@@ -0,0 +1,17 @@
+<template>
+  <button class="button" type="button">
+    <slot />
+  </button>
+</template>
+<style>
+.button {
+  color: black;
+  border: 5px solid #ffe6f2;
+  border-radius: 10px;
+  padding: 15px;
+  font-size: 15px;
+  font-family: "Acme", sans-serif;
+  cursor: pointer;
+  text-transform: uppercase;
+}
+</style>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -3,6 +3,15 @@
     <div class="title">
       <a href="index.html">Recipe Box</a>
       <i class="fas fa-utensils"></i>
+      <Button>Add Recipe</Button>
     </div>
   </header>
 </template>
+<script>
+import Button from "./Button";
+export default {
+  components: {
+    Button
+  }
+};
+</script>
