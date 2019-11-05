3cc3d772f84d49ff3eae75998fe3a72a7912d76b
Liudmyla Liashuk <lyudmyla.horbyk@gmail.com>
2019-11-05 22:44:45 +0200

    update button style in header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -3,8 +3,8 @@
     <div class="title">
       <a href="index.html">Recipe Box</a>
       <i class="fas fa-utensils"></i>
-      <Button v-on:click.native="onButtonClick">Add Recipe</Button>
     </div>
+    <Button class="header-button" v-on:click.native="onButtonClick">Add Recipe</Button>
   </header>
 </template>
 <script>
@@ -21,3 +21,39 @@ export default {
   }
 };
 </script>
+<style>
+header {
+  display: flex;
+  align-items: center;
+  background-color: #ff80aa;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  padding-top: 20px;
+  padding-bottom: 20px;
+}
+header .title {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+header .title a {
+  color: white;
+  font-family: "Acme", sans-serif;
+  font-size: 35px;
+  margin: 0 25px 0 25px;
+  text-decoration: none;
+}
+header .title .fas {
+  color: white;
+  font-size: 30px;
+}
+header .header-button {
+  margin-right: 25px;
+}
+@media (max-width: 405px) {
+  header {
+    justify-content: center;
+    flex-direction: column;
+  }
+}
+</style>
