a02d844cb6af65e265bff8d50114ed3e7fcd5964
Liudmyla Liashuk <lyudmyla.horbyk@gmail.com>
2019-11-19 00:19:50 +0200

    implement delete button on the recipe-page

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -4,6 +4,7 @@
       <router-link :to="{name: 'Home'}">Recipe Box</router-link>
       <i class="fas fa-utensils"></i>
     </div>
+
     <Button class="header-button" v-on:click.native="onButtonClick">Add Recipe</Button>
   </header>
 </template>
