25fa3f4177498f7f4fa3663b313399373e3592fc
Liudmyla Liashuk <lyudmyla.horbyk@gmail.com>
2020-02-06 13:52:24 +0200

    fixed cards block

--- src/components/RecipeCover.vue
+++ src/components/RecipeCover.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="recipe">
+    <div class="wrapper-img">
     <img :src="recipe.url" :alt="recipe.title" />
+    </div>
     <p>{{ recipe.title }}</p>
   </div>
 </template>
@@ -24,12 +26,19 @@ export default {
     margin: 20px;
     img {
       width: 100%;
+      height: auto
     }
     p {
       color: black;
       font-family: "Acme", sans-serif;
       font-size: 25px;
       text-align: center;
+      height: 100px;
+    }
+    .wrapper-img{
+      height: 100%;
+      width: 100%;
+      display: block;
     }
   }
 }
